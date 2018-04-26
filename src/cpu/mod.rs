e323523d49f1eb113274e01019107662fa522467
Stefan <falken@umich.edu>
2018-04-25 21:29:24 -0400

    struggling with understanding how carry should work in the different modes

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -62,16 +62,23 @@ impl IndexMut<usize> for CPUMem {
 impl CPU {
     fn immediate   (&self, val : u8)  -> u8  { val }
     fn absolute    (&self, val : u16) -> u16 { val }
-    fn absolute_x  (&self, val : u16) -> u16 { val + self.x as u16 }
-    fn absolute_y  (&self, val : u16) -> u16 { val + self.y as u16 }
+    // TODO figure out if adding the index actually should wrap
+    fn absolute_x  (&self, val : u16) -> u16 { val.wrapping_add(self.x as u16) }
+    fn absolute_y  (&self, val : u16) -> u16 { val.wrapping_add(self.y as u16) }
     fn zero_page   (&self, val : u8)  -> u16 { val as u16 }
-    fn zero_page_x (&self, val : u8)  -> u16 { val as u16 + self.x as u16 }
-    fn zero_page_y (&self, val : u8)  -> u16 { val as u16 + self.y as u16 }
-    fn indirect_x  (&self, val : u16) -> u16 { self.indirect(val + self.x as u16) }
-    fn indirect_y  (&self, val : u16) -> u16 { self.indirect(val) + self.y as u16 }
+    fn zero_page_x (&self, val : u8)  -> u16 { val.wrapping_add(self.x) as u16 }
+    fn zero_page_y (&self, val : u8)  -> u16 { val.wrapping_add(self.y) as u16 }
+    fn indirect_x  (&self, val : u8)  -> u16 {
+        let a = val.wrapping_add(self.x);
+        self.indirect(a as u16)
+    }
+    fn indirect_y  (&self, val : u8)  -> u16 {
+        // self.indirect(val) + self.y as u16
+        0
+    }
     fn indirect    (&self, val : u16) -> u16 {
         let i = val as usize;
-        self.mem[i] as u16 + (self.mem[i + 1] as u16) << 8
+        self.mem[i] as u16 + ((self.mem[i + 1] as u16) << 8)
     }
 
     pub fn step (&mut self) {
@@ -126,38 +133,81 @@ mod tests {
     #[test]
     fn lda() {
         let mut c = CPU::new();
-        c.lda(0);
-        assert_eq!(c.a, 0);
+        c.lda(0x00);
+        assert_eq!(c.a, 0x00);
         assert_eq!(c.flags.z, true);
         assert_eq!(c.flags.n, false);
-        c.lda(8);
-        assert_eq!(c.a, 8);
-        assert_eq!(c.flags.z, false);
-        assert_eq!(c.flags.n, false);
-        c.lda(255);
-        assert_eq!(c.a, 255);
+        c.lda(0xFF);
+        assert_eq!(c.a, 0xFF);
         assert_eq!(c.flags.z, false);
         assert_eq!(c.flags.n, true);
     }
     #[test]
     fn addr_modes() {
+
         let mut c = CPU::new();
-        assert_eq!(c.immediate(0u8), 0u8);
-        assert_eq!(c.immediate(255u8), 255u8);
-        assert_eq!(c.absolute(0u16), 0u16);
-        assert_eq!(c.absolute(65535u16), 65535u16);
+        assert_eq!(c.immediate(0x00_u8), 0x00_u8);
+        assert_eq!(c.immediate(0xFF_u8), 0xFF_u8);
+        assert_eq!(c.absolute(0x0000_u16), 0x0000_u16);
+        assert_eq!(c.absolute(0xFFFF_u16), 0xFFFF_u16);
 
-        // fn absolute_x  (&self, val : u16) -> u16
-        c.x = 0;
-        assert_eq!(c.absolute_x(0u16), 0u16);
-        assert_eq!(c.absolute_x(65535u16), 65535u16);
+        c.x = 0x00;
+        assert_eq!(c.absolute_x(0x0000_u16), 0x0000_u16);
+        assert_eq!(c.absolute_x(0xFFFF_u16), 0xFFFF_u16);
+        c.x = 0xFF;
+        assert_eq!(c.absolute_x(0x0000_u16), 0x00FF_u16);
+        assert_eq!(c.absolute_x(0xFFFF_u16), 0x00FE_u16);
 
+        c.y = 0x00;
+        assert_eq!(c.absolute_y(0x0000_u16), 0x0000_u16);
+        assert_eq!(c.absolute_y(0xFFFF_u16), 0xFFFF_u16);
+        c.y = 0xFF;
+        assert_eq!(c.absolute_y(0x0000_u16), 0x00FF_u16);
+        assert_eq!(c.absolute_y(0xFFFF_u16), 0x00FE_u16);
 
-        // fn absolute_y  (&self, val : u16) -> u16
-        // fn zero_page   (&self, val : u8)  -> u16
-        // fn zero_page_x (&self, val : u8)  -> u16
-        // fn zero_page_y (&self, val : u8)  -> u16
-        // fn indirect_x  (&self, val : u16) -> u16
+        assert_eq!(c.zero_page(0x00_u8), 0x0000_u16);
+        assert_eq!(c.zero_page(0xFF_u8), 0x00FF_u16);
+
+        c.x = 0x00;
+        assert_eq!(c.zero_page_x(0x00_u8), 0x0000_u16);
+        assert_eq!(c.zero_page_x(0xFF_u8), 0x00FF_u16);
+        c.x = 0xFF;
+        assert_eq!(c.zero_page_x(0x00_u8), 0x00FF_u16);
+        // this is an interesting case: zero page addressing with index
+        // specifies (according to MOS datasheet) essentially that
+        // the index is added to the argument before it is extended to
+        // 16 bits, so any carry from that addition is dropped
+        assert_eq!(c.zero_page_x(0xFF_u8), 0x00FE_u16);
+
+        c.y = 0x00;
+        assert_eq!(c.zero_page_y(0x00_u8), 0x0000_u16);
+        assert_eq!(c.zero_page_y(0xFF_u8), 0x00FF_u16);
+        c.y = 0xFF;
+        assert_eq!(c.zero_page_y(0x00_u8), 0x00FF_u16);
+        assert_eq!(c.zero_page_y(0xFF_u8), 0x00FE_u16);
+
+        c.mem[0x0] = 0xA;
+        c.mem[0x1] = 0xB;
+        c.mem[0x2] = 3;
+        c.mem[0xFE] = 1;
+        c.mem[0xFF] = 2;
+
+        assert_eq!(c.indirect(0x0000_u16), 0x0B0A_u16);
+        assert_eq!(c.indirect(0x00FE_u16), 0x0201_u16);
+
+        // TODO figure out if this should pass
+        // assert_eq!(c.indirect(0x00FF_u16), 0x0A02_u16);
+
+        c.x = 0x00;
+        assert_eq!(c.indirect_x(0x00_u8), 0x0B0A_u16);
+        assert_eq!(c.indirect_x(0xFE_u8), 0x0201_u16);
+        c.x = 0xFE;
+        assert_eq!(c.indirect_x(0x02_u8), 0x0B0A_u16);
+        assert_eq!(c.indirect_x(0x00_u8), 0x0201_u16);
+
+        c.y = 0x00;
+        assert_eq!(c.indirect_y(0x00_u8), 0x0B0A_u16);
+
         // fn indirect_y  (&self, val : u16) -> u16
         // fn indirect    (&self, val : u16) -> u16
     }
