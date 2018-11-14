bbf4aff2c432418ee3a2c64c5eadd13d5f3da5b9
Stefan <falken@umich.edu>
2018-11-14 17:39:38 -0500

    working on audio

--- src/audio.rs
+++ src/audio.rs
@@ -1,9 +1,18 @@
+use ::sdl2::audio::{ AudioSpecDesired, AudioQueue };
 
-pub struct EmulatorAudio {}
+pub struct EmulatorAudio {
+    queue : AudioQueue<f32>,
+}
 
 impl EmulatorAudio {
     pub fn new(sdl_context : &::Sdl) -> EmulatorAudio {
+        let spec = AudioSpecDesired {
+            freq : None,
+            channels : None,
+            samples : None,
+        };
         EmulatorAudio {
+            queue : sdl_context.audio().unwrap().open_queue(None, &spec).unwrap(),
         }
     }
 }
